6313b4aa0267ade5e92fb153881b164c15201905
Yuriy Levytskyi <yuriy.dozer@gmail.com>
2022-12-29 06:57:38 +0200

    p4: modify command.sh for delete older tar file

--- linux_p2/script.sh
+++ linux_p2/script.sh
@@ -6,6 +6,9 @@ curent_date=$(date +%d-%m-%Y);
 #select all txt file in directory
 directory=$(ls *.txt); 
 
+#delete all tar.gz file older then 30min
+find -name "*tar.gz" -mmin +30 -type f -exec rm -f {} \;
+
 #arhive all txt file with modify name in curent directory
 for file in $directory;
 do
