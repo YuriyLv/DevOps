e215b1771f9d5af7f6901e9927797c3cdb9a6ef6
Yuriy Levytskyi <yuriy.dozer@gmail.com>
2022-12-29 08:12:16 +0200

    p5: modify command.sh for make log files

--- linux_p2/script.sh
+++ linux_p2/script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#init log file
+log=backup.log
+err=err_backup.log
+
 #take curent date for name modify
 curent_date=$(date +%d-%m-%Y);
 
@@ -7,10 +11,12 @@ curent_date=$(date +%d-%m-%Y);
 directory=$(ls *.txt); 
 
 #delete all tar.gz file older then 30min
-find -name "*tar.gz" -mmin +30 -type f -exec rm -f {} \;
+find -name "*tar.gz" -mmin +1 -type f -exec rm -fv {} \; 2>>$err >>$log
 
 #arhive all txt file with modify name in curent directory
 for file in $directory;
 do
-    tar -czvf $file"_"$curent_date.tar.gz ./$file;
+    tar -czvf $file"_"$curent_date.tar.gz ./$file 2>>$err >>$log 
+    #sleep 0
 done
+
